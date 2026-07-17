0cf94e58681dadd9feedb31af8d71aa993af810b
tekxaidev <tekxaidev@Tekxai-Zia.local>
2026-07-17 16:11:30 +0500

    fix: use production API fallback

--- src/lib/apiConfig.ts
+++ src/lib/apiConfig.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://192.168.0.231:3022/';
+export const BASE_URL =  import.meta.env.VITE_API_BASE_URL || 'https://api.tekxai.services/';
